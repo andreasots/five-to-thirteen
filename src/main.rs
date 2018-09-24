6a128c8b94ac788847fad064c3dfd0f46afb21c7
Andreas Ots <qrpth@qrpth.eu>
2018-09-24 22:29:45 +0300

    Fearless Concurrency! Also a fixed parser.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+extern crate rayon;
+
+use rayon::prelude::*;
+
 mod parser;
 
 fn main() {
-    let mut buf = [0u8; 14];
-    'outer: for len in 1usize..14 {
+    for len in 1usize..14 {
         println!("Searching for max len {}", len);
-        for mut i in 0..parser::BASE.pow(len as u32) {
-            for digit in 0..len {
-                buf[digit] = (i % parser::BASE) as u8;
-                i /= parser::BASE;
-            }
-            if parser::expr(&buf[0..len]) == Ok((13.0, &[])) {
-                println!("{:?}", &buf[0..len]);
-                break 'outer;
-            }
-        }
+        (0..parser::BASE.pow(len as u32)).into_par_iter()
+            .for_each(|mut i| {
+                let mut buf = [0u8; 14];
+                for digit in 0..len {
+                    buf[digit] = (i % parser::BASE) as u8;
+                    i /= parser::BASE;
+                }
+                if parser::expr(&buf[0..len]) == Ok((13.0, &[])) {
+                    println!("{}", parser::format(&buf[0..len]));
+                }
+            });
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -7,35 +7,40 @@ pub const LPAREN: u8 = 5;
 pub const RPAREN: u8 = 6;
 pub const BASE: u64 = 7;
 
-// expr ::= prod PLUS expr | prod MINUS expr | prod
-pub fn expr(input: &[u8]) -> Result<(f64, &[u8]), ()> {
-    let (left, input) = prod(input)?;
-    match input.get(0) {
-        Some(&ADD) => {
-            let (right, input) = expr(&input[1..])?;
-            Ok((left + right, input))
-        },
-        Some(&SUB) => {
-            let (right, input) = expr(&input[1..])?;
-            Ok((left - right, input))
-        },
-        _ => Ok((left, input))
+macro_rules! try {
+    ($term:ident($input:ident)) => {{
+        let (res, input) = $term($input)?;
+        $input = input;
+        res
+    }};
+    ($term:ident(&$input:ident[$index:expr])) => {{
+        let (res, input) = $term(&$input[$index])?;
+        $input = input;
+        res
+    }};
+}
+
+// expr ::= prod ([ADD SUB] prod)*
+pub fn expr(mut input: &[u8]) -> Result<(f64, &[u8]), ()> {
+    let mut accumulator = try!(prod(input));
+    loop {
+        match input.get(0) {
+            Some(&ADD) => accumulator += try!(prod(&input[1..])),
+            Some(&SUB) => accumulator -= try!(prod(&input[1..])),
+            _ => return Ok((accumulator, input)),
+        }
     }
 }
 
-// prod ::= num MUL prod | num DIV prod | num
-pub fn prod(input: &[u8]) -> Result<(f64, &[u8]), ()> {
-    let (left, input) = num(input)?;
-    match input.get(0) {
-        Some(&MUL) => {
-            let (right, input) = prod(&input[1..])?;
-            Ok((left * right, input))
-        },
-        Some(&DIV) => {
-            let (right, input) = prod(&input[1..])?;
-            Ok((left / right, input))
-        },
-        _ => Ok((left, input)),
+// prod ::= num ([MUL DIV] num)*
+pub fn prod(mut input: &[u8]) -> Result<(f64, &[u8]), ()> {
+    let mut accumulator = try!(num(input));
+    loop {
+        match input.get(0) {
+            Some(&MUL) => accumulator *= try!(num(&input[1..])),
+            Some(&DIV) => accumulator /= try!(num(&input[1..])),
+            _ => return Ok((accumulator, input)),
+        }
     }
 }
 
@@ -55,7 +60,42 @@ pub fn num(input: &[u8]) -> Result<(f64, &[u8]), ()> {
     }
 }
 
+pub fn format(input: &[u8]) -> String {
+    let mut ret = String::new();
+
+    for c in input {
+        match *c {
+            FIVE => ret.push_str("5"),
+            ADD => ret.push_str(" + "),
+            SUB => ret.push_str(" - "),
+            MUL => ret.push_str(" * "),
+            DIV => ret.push_str(" / "),
+            LPAREN => ret.push_str("("),
+            RPAREN => ret.push_str(")"),
+            _ => unreachable!(),
+        }
+    }
+
+    ret
+}
+
 #[test]
 fn minimal_conjecture() {
     assert_eq!(expr(&[LPAREN, FIVE, MUL, FIVE, MUL, FIVE, ADD, FIVE, RPAREN, DIV, LPAREN, FIVE, ADD, FIVE, RPAREN]), Ok((13.0f64, &[][..])));
+}
+
+#[test]
+fn all_len_13_candidates() {
+    assert_eq!(expr(&[LPAREN, FIVE, ADD, FIVE, ADD, FIVE, RPAREN, DIV, FIVE, ADD, FIVE, ADD, FIVE]), Ok((13.0, &[][..])));
+    assert_eq!(expr(&[FIVE, SUB, FIVE, DIV, FIVE, ADD, FIVE, DIV, FIVE, SUB, FIVE, ADD, FIVE]), Ok((5.0, &[][..])));
+    assert_eq!(expr(&[FIVE, SUB, LPAREN, FIVE, ADD, FIVE, RPAREN, DIV, FIVE, SUB, FIVE, ADD, FIVE]), Ok((3.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, LPAREN, FIVE, ADD, FIVE, ADD, FIVE, RPAREN, DIV, FIVE, ADD, FIVE]), Ok((13.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, FIVE, SUB, FIVE, DIV, FIVE, ADD, FIVE, DIV, FIVE, SUB, FIVE]), Ok((5.0, &[][..])));
+    assert_eq!(expr(&[FIVE, SUB, FIVE, DIV, FIVE, SUB, FIVE, SUB, FIVE, DIV, FIVE, SUB, FIVE]), Ok((-7.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, FIVE, SUB, LPAREN, FIVE, ADD, FIVE, RPAREN, DIV, FIVE, SUB, FIVE]), Ok((3.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, FIVE, ADD, FIVE, SUB, FIVE, DIV, FIVE, ADD, FIVE, DIV, FIVE]), Ok((15.0, &[][..])));
+    assert_eq!(expr(&[FIVE, SUB, FIVE, DIV, FIVE, SUB, FIVE, ADD, FIVE, SUB, FIVE, DIV, FIVE]), Ok((3.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, FIVE, SUB, FIVE, DIV, FIVE, SUB, FIVE, SUB, FIVE, DIV, FIVE]), Ok((3.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, FIVE, ADD, LPAREN, FIVE, ADD, FIVE, ADD, FIVE, RPAREN, DIV, FIVE]), Ok((13.0, &[][..])));
+    assert_eq!(expr(&[FIVE, ADD, FIVE, ADD, FIVE, SUB, LPAREN, FIVE, ADD, FIVE, RPAREN, DIV, FIVE]), Ok((13.0, &[][..])));
 }
